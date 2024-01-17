61f8e8c90e94d5df0dac6a056f6cf8bad0042b9c
Jackson <jackson.njihia@student.moringaschool.com>
2024-01-17 03:25:41 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    var textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+  
